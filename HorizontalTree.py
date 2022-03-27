a15de60482fb5fd0c30f6e115f32cb4f1f0ac8d0
presentsail <83531072+presentsail@users.noreply.github.com>
2022-03-27 12:54:29 -0400

    Used ancestors to make `printtree` functional
     - Added `ancestors` method to `Node` class that returns a list of all a node's ancestors (not self-inclusive).
     - Moved `build` function definition outside of `printtree`.
     - Method `printtree` no longer prints extra edge as if another child is present.

--- HorizontalTree.py
+++ HorizontalTree.py
@@ -28,37 +28,61 @@ class Node:
             return self.parent.left == self
         return False
 
-def printtree(startingnode, depth):
-    def build(*args):
-        print(*args, sep='', end='')
+    def ancestors(self):
+        ancestors = []
+        parent = self.parent
+        while parent is not None:
+            ancestors.append(parent)
+            parent = parent.parent
+        return ancestors
 
-    for d in range(depth-1):
-        build('|   ')
+
+def build(*args):
+    print(*args, sep='', end='')
+
+def printtree(startingnode, depth=0):
+    def drawprevedges():
+        for ances in reversed(startingnode.ancestors()[:-1]):
+            if ances.isleft():
+                build('    ')
+            else:
+                build('|   ')
+    drawprevedges()
 
     if depth > 0:
         build('+–––')
     print(startingnode.element)
 
     if not startingnode.haschildren():
+        if startingnode.isleft():
+            drawprevedges()
+            print()
         return
 
     for child in reversed(startingnode.children()):
         printtree(child, depth+1)
 
-    for d in range(depth):
-        build('|   ')
-    print()
 
-root = Node(None, 0)
-nR = root.setright(1)
-nL = root.setleft(2)
-nRR = nR.setright(3)
-nRL = nR.setleft(4)
-nLR = nL.setright(5)
-nLL = nL.setleft(6)
-nRRR = nRR.setright(7)
-nRRL = nRR.setleft(8)
-nRRL.setright(9)
-nRRL.setleft(10)
+if __name__ == '__main__':
+    root = Node(None, 0)
+    nR = root.setright(1)
+    nL = root.setleft(2)
 
-printtree(root, 0)
+    nRR = nR.setright(3)
+    nRL = nR.setleft(4)
+    nLR = nL.setright(5)
+    nLL = nL.setleft(6)
+
+    nRRR = nRR.setright(7)
+    nRRL = nRR.setleft(8)
+    nRRL.setright(9)
+    nRRL.setleft(10)
+    nRL.setright(11)
+    nRL.setleft(12)
+    nLRR = nLR.setright(13)
+    nLR.setleft(14)
+    nLL.setright(15)
+    nLL.setleft(16)
+
+    printtree(root)
+    
